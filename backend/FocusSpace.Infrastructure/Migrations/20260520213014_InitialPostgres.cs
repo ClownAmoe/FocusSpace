7c395e1d3350c155964aab05487f2525d90df772
Bohdana <bgdnstd@gmail.com>
2026-05-21 00:30:56 +0300

    updated bd

---
+++ backend/FocusSpace.Infrastructure/Migrations/20260520213014_InitialPostgres.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FocusSpace.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialPostgres : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tasks_Planets_PlanetId",
+                table: "Tasks");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tasks_Planets_PlanetId",
+                table: "Tasks",
+                column: "PlanetId",
+                principalTable: "Planets",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tasks_Planets_PlanetId",
+                table: "Tasks");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tasks_Planets_PlanetId",
+                table: "Tasks",
+                column: "PlanetId",
+                principalTable: "Planets",
+                principalColumn: "Id");
+        }
+    }
+}
